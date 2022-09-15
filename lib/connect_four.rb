1d4aa8546e7443b5625a54ea8db6ba4dced817bd
Schifferking <schifferking@hotmail.com>
2022-09-15 13:07:09 -0500

    Add prompt method
    This method has two optional arguments: min and max. Min is set to 0
    and max is set to 7; these values are used in a statement where asks
    the player for a number between these two numbers, because the game
    will ask two numbers for the coordinates of the grid in which to place
    a token.

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -10,4 +10,8 @@ class ConnectFour
     @player_one = Player.new("\u26aa")
     @player_two = Player.new("\u26ab")
   end
+
+  def prompt(min = 0, max = 7)
+    print "Please enter a number between #{min} and #{max}: "
+  end
 end
