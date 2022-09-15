52a4eaaca8cb269902bb90c8c6eef727b4a6fe83
Schifferking <schifferking@hotmail.com>
2022-09-15 12:47:28 -0500

    Add code to initialize
    The ConnectFour class now has two players, one with a different circle
    and a board added previously. Also there are methods to access the
    player's tokens and modify the board.

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,7 +1,13 @@
 require_relative '../lib/board'
+require_relative '../lib/player'
 
 class ConnectFour
+  attr_reader :player_one, :player_two
+  attr_accessor :board
+
   def initialize
     @board = Board.new
+    @player_one = Player.new("\u26aa")
+    @player_two = Player.new("\u26ab")
   end
 end
