f5adfd5f72f3d694db3c7a179b19518ba266e201
Schifferking <schifferking@hotmail.com>
2022-09-20 13:31:03 -0500

    Made changes to Board class
    First, added an attr_accessor to the grid instance variable.
    
    The initialize method now only has the create_board method.
    
    The create_board method now creates a 2d array of empty string values.
    
    Added a new cell_empty? method that has two parameters: row and column,
    values that are used to access a cell in the grid. The method then
    returns true if this cell is an empty string, false otherwise.

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,15 @@
 class Board
+  attr_accessor :grid
+
   def initialize
-    @grid = nil
+    create_board
   end
 
   def create_board
-    @grid = Array.new(6) { Array.new(7) { nil } }
+    @grid = Array.new(6) { Array.new(7) { '' } }
+  end
+
+  def cell_empty?(row, column)
+    @grid[row][column].empty?
   end
 end
